8acdb27e121b2fb2f0b927e9a9b762899850ce7d
yogaajie2 <yogaajie2@gmail.com>
2022-02-25 08:58:34 +0700

    style(main.js): add missing semi-colons

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from './App.vue'
-import router from './router'
-import './assets/tailwind.css'
+import App from './App.vue';
+import router from './router';
+
+import './assets/tailwind.css';
 
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
 
-app.mount('#app')
+app.use(createPinia());
+app.use(router);
+
+app.mount('#app');
