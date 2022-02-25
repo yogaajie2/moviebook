4cdfcb59a2ca21d1774e0144fdd7a6bb202b924c
yogaajie2 <yogaajie2@gmail.com>
2022-02-25 09:28:57 +0700

    refactor(TheHeader.vue): update TheHeader UI

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,5 +1,37 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
 <template>
-  <header class="container py-2 bg-primary-light">
-    <h1>MovieBook</h1>
+  <header class="py-2 bg-primary-light">
+    <div class="container flex justify-between items-center">
+      <font-awesome-icon
+        icon="bars"
+        size="lg"
+        class="lg:hidden"
+      />
+  
+      <div class="flex items-center gap-8">
+        <router-link to="/">
+          <h1 class="text-center lg:text-left">
+            MovieBook
+          </h1>
+        </router-link>
+  
+        <nav class="hidden font-heading space-x-4 lg:block">
+          <router-link to="/movies">
+            Movies
+          </router-link>
+          <router-link to="/series">
+            Series
+          </router-link>
+        </nav>
+      </div>
+      
+      <font-awesome-icon
+        icon="magnifying-glass"
+        size="lg"
+      />
+    </div>
   </header>
 </template>
