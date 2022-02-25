f6f98a49b436e5dd2ef91f769daa6a88a8b1bdfa
yogaajie2 <yogaajie2@gmail.com>
2022-02-25 09:28:15 +0700

    feat(main.js): add fontawesome

--- src/main.js
+++ src/main.js
@@ -1,16 +1,20 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 import App from './App.vue';
 import router from './router';
 
 import './assets/tailwind.css';
 
-const app = createApp(App)
+library.add(faBars, faMagnifyingGlass);
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.component('FontAwesomeIcon', FontAwesomeIcon);
 
 app.mount('#app');
