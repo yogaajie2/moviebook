a6b9547c19cf1e553b2a673b3ba1b86d8f656c33
yogaajie2 <yogaajie2@gmail.com>
2022-03-01 12:06:04 +0700

    chore(placeholder): move placeholder data to a new file
    just in case

---
+++ placeholder.js
@@ -0,0 +1,57 @@
+const movies = ref([
+  {
+    poster: 'https://via.placeholder.com/150x225',
+    title: 'Movie 1',
+    year: 2022,
+    rating: 69,
+  },
+
+  {
+    poster: 'https://via.placeholder.com/150x225',
+    title: 'Movie 2',
+    year: 2021,
+    rating: 69,
+  },
+
+  {
+    poster: 'https://via.placeholder.com/150x225',
+    title: 'Movie 3',
+    year: 2020,
+    rating: 69,
+  },
+
+  {
+    poster: 'https://via.placeholder.com/150x225',
+    title: 'Movie 4',
+    year: 2019,
+    rating: 69,
+  },
+
+  {
+    poster: 'https://via.placeholder.com/150x225',
+    title: 'Movie 5',
+    year: 2018,
+    rating: 69,
+  },
+
+  {
+    poster: 'https://via.placeholder.com/150x225',
+    title: 'Movie 6',
+    year: 2017,
+    rating: 69,
+  },
+
+  {
+    poster: 'https://via.placeholder.com/150x225',
+    title: 'Movie 7',
+    year: 2016,
+    rating: 69,
+  },
+
+  {
+    poster: 'https://via.placeholder.com/150x225',
+    title: 'Movie 8',
+    year: 2015,
+    rating: 69,
+  },
+]);
