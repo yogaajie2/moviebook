2068a1434ef2e5f8fcd1c8889229dd5b666a418e
yogaajie2 <yogaajie2@gmail.com>
2022-03-02 11:03:34 +0700

    feat(TitleList): add new TitleList component

---
+++ src/components/TitleList.vue
@@ -0,0 +1,40 @@
+<template lang="javascript">
+  <section class="space-y-2">
+    <h2>Popular</h2>
+
+    <div class="scrollbar-thin scrollbar-thumb-primary-lighter scrollbar-track-primary-light flex overflow-x-scroll pb-4 gap-6">
+      <div
+        v-for="movie in movies.results"
+        :key="movie.id"
+        class="basis-2/5 shrink-0 space-y-2 md:basis-1/4 xl:basis-1/6"
+      >
+        <div class="relative">
+          <img
+            :src="`https://image.tmdb.org/t/p/w300/${movie.poster_path}`"
+            class="rounded-lg"
+          >
+
+          <p class="absolute flex items-center -right-2 top-2 px-2 py-1 rounded font-bold text-sm bg-primary-light gap-1 shadow">
+            <font-awesome-icon
+              icon="star"
+              size="sm"
+              class="text-tertiary"
+            />
+
+            {{ movie.vote_average }}
+          </p>
+        </div>
+
+        <div>
+          <p class="font-bold">
+            {{ movie.title }}
+          </p>
+
+          <p class="text-sm">
+            {{ getYear(movie.release_date) }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
