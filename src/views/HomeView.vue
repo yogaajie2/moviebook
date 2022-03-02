d8c3419b7897ccd0a0788a33fbe8059dffe2f982
yogaajie2 <yogaajie2@gmail.com>
2022-03-02 11:02:45 +0700

    style(HomeView): add missing whitespace

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,6 +16,7 @@ onMounted(async () => {
 
     // Upcoming
     // .get('https://api.themoviedb.org/3/movie/upcoming?api_key=70d100dc02aa86f434714200877bff47&language=en-US&page=1&region=ID')
+    
     .then(response => {
       movies.value = response.data;
     });
