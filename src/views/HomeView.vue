24ea74b3ce0a6d3fa2baf319a9c83a1a62aacd2e
yogaajie2 <yogaajie2@gmail.com>
2022-03-08 16:26:38 +0700

    feat(HomeView.vue): update HomeView UI

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,43 +1,76 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
-import TitleItem from '../components/TitleItem.vue';
-const titles = ref([]);
+import ItemList from '../components/ItemList.vue';
+
+const API_KEY = "70d100dc02aa86f434714200877bff47";
+
+const lists = [
+  {
+    id: 1,
+    name: 'Now Playing',
+    items: ref({}),
+  },
+
+  {
+    id: 2,
+    name: 'Popular',
+    items: ref({}),
+  },
+
+  {
+    id: 3,
+    name: 'Upcoming',
+    items: ref({}),
+  },
+
+  {
+    id: 4,
+    name: 'Top Rated',
+    items: ref({}),
+  },
+];
 
 onMounted(async () => {
   await axios
     // Now Playing
-    // .get('https://api.themoviedb.org/3/movie/now_playing?api_key=70d100dc02aa86f434714200877bff47&language=en-US&page=1&region=ID')
-
-    // Popular
-    .get('https://api.themoviedb.org/3/movie/popular?api_key=70d100dc02aa86f434714200877bff47&language=en-US&page=1')
-
-    // Top Rated
-    // .get('https://api.themoviedb.org/3/movie/top_rated?api_key=70d100dc02aa86f434714200877bff47&language=en-US&page=1')
-
-    // Upcoming
-    // .get('https://api.themoviedb.org/3/movie/upcoming?api_key=70d100dc02aa86f434714200877bff47&language=en-US&page=1&region=ID')
-    
+    .get("https://api.themoviedb.org/3/movie/now_playing?api_key=" + API_KEY + "&language=en-US&page=1&region=ID")
     .then(response => {
-      titles.value = response.data;
+      lists[0].items.value = response.data;
+    });
+
+  await axios
+    // Popular
+    .get("https://api.themoviedb.org/3/movie/popular?api_key=" + API_KEY + "&language=en-US&page=1")
+    .then(response => {
+      lists[1].items.value = response.data;
+    });
+
+  await axios
+    // Upcoming
+    .get("https://api.themoviedb.org/3/movie/upcoming?api_key=" + API_KEY + "&language=en-US&page=1&region=ID")
+    .then(response => {
+      lists[2].items.value = response.data;
+    });
+
+  await axios
+    // Top Rated
+    .get("https://api.themoviedb.org/3/movie/top_rated?api_key=" + API_KEY + "&language=en-US&page=1")
+    .then(response => {
+      lists[3].items.value = response.data;
     });
 });
 </script>
 
 <template>
   <main class="py-4">
-    <div class="container">
-      <section class="space-y-2">
-        <h2>Popular</h2>
-  
-        <div class="scrollbar-thin scrollbar-thumb-primary-lighter scrollbar-track-primary-light flex overflow-x-scroll pb-4 gap-6">
-          <TitleItem
-            v-for="title in titles.results"
-            :key="title.id"
-            :title="title"
-          />
-        </div>
-      </section>
+    <div class="container space-y-4">
+      <ItemList
+        v-for="list in lists"
+        :key="list.id"
+        :name="list.name"
+        :items="list.items.value"
+      />
     </div>
   </main>
 </template>
