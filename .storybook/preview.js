20dd6130adedca2cdc63ce0039a55d07b19ec32f
yogaajie2 <yogaajie2@gmail.com>
2022-03-09 15:22:30 +0700

    build(storybook): import fontawesome icons

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,13 @@
+import { app } from '@storybook/vue3';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBars, faMagnifyingGlass, faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 import '../src/assets/tailwind.css';
+
+library.add(faBars, faMagnifyingGlass, faStar);
+
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
