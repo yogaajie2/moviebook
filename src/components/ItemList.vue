78271b80fcd23f14648563b6f84e1e82d2c76104
yogaajie2 <yogaajie2@gmail.com>
2022-03-15 10:46:20 +0700

    fix(ItemList): increase bottom padding
    change pb-4 to pb-6

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -23,7 +23,7 @@ defineProps({
   <section class="space-y-2">
     <h2>{{ name }}</h2>
 
-    <div class="scrollbar-thin scrollbar-thumb-primary-lighter scrollbar-track-primary-light flex overflow-x-scroll pb-4 gap-6">
+    <div class="scrollbar-thin scrollbar-thumb-primary-lighter scrollbar-track-primary-light flex overflow-x-scroll pb-6 gap-6">
       <TheItem
         v-for="item in items.results"
         :key="item.id"
